c519f7008fc4e64acc502480f1204d7e56847303
Roshan R Chandar <43182697+Roshan-R@users.noreply.github.com>
2021-10-17 18:29:34 +0530

    feature : send multiple files (#1)

--- telegram-send.sh
+++ telegram-send.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-telegram-desktop -sendpath "$(readlink -f "$1")"
+
+arr=()
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+for var in "$@"
+do
+    path=$(readlink -f $var)
+    arr+=($path)
+done
+
+telegram-desktop -sendpath "${arr[@]}"
 wmctrl -x -a Telegram.TelegramDesktop
+
+IFS=$SAVEIFS
